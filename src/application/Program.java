b934b4d5283f0978ca13c11e786fe31e95cb7ddf
Djalma <djalma.ant@gmail.com>
2021-12-05 15:13:39 -0300

    Interface Comparable

---
+++ src/application/Program.java
@@ -0,0 +1,27 @@
+package application;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import entities.Product;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		List<Product> list = new ArrayList<>();
+		
+		list.add(new Product("Playstation 5", 5999.90));
+		list.add(new Product("Iphone 11", 4999.99));
+		list.add(new Product("TV Samsung", 2999.50));
+		
+		Collections.sort(list);
+		
+		for (Product p : list) {
+			System.out.println(p);
+		}
+
+	}
+
+}
